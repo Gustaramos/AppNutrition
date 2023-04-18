43913b300d47b1dc5fb466d7b9fe7dab0378dcfd
Cataflam <guram.sil@outlook.com>
2023-04-18 09:46:34 -0300

    Commit att

--- src/main/java/br/com/nutrition/service/CadastroNutricionista.java
+++ src/main/java/br/com/nutrition/service/CadastroNutricionista.java
@@ -1,7 +1,5 @@
 package br.com.nutrition.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.nutrition.model.Nutricionista;
@@ -11,7 +9,6 @@ import br.com.nutrition.repository.NutricionistaRepository;
 public class CadastroNutricionista {
 	@Autowired
 	private NutricionistaRepository nutricionistaRepository;
-	
 	
 	//salvar o cadastro do nutricionista no servidor
 	public void cadastro(Nutricionista nutricionista) {
